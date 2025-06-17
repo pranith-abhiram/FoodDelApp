548aefb9052de24c3a8f988903a99c3cf4321d0f
pranith-abhiram <pranithabhiramg@gmail.com>
2025-06-17 18:29:00 +0530

    added FoodDisplay

--- forntend/src/components/ExploreMenu/ExploreMenu.jsx
+++ forntend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
-const ExploreMenu = () => {
+const ExploreMenu = ({category,setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore our menu</h1>
@@ -9,8 +9,8 @@ const ExploreMenu = () => {
       <div className='explore-menu-list'>
         {menu_list.map((item,index)=>{
             return(
-                <div className='explore-menu-list-item' key={index}>
-                    <img src={item.menu_image}/>
+                <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} className='explore-menu-list-item' key={index}>
+                    <img className={category===item.menu_name?"active":""} src={item.menu_image}/>
                     <p>{item.menu_name}</p>
                 </div>   
             )
