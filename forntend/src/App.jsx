b0e294d578dd2c108040750a397c47923eb365af
pranith-abhiram <pranithabhiramg@gmail.com>
2025-06-18 13:39:33 +0530

    added responsive feature

--- forntend/src/App.jsx
+++ forntend/src/App.jsx
@@ -4,16 +4,24 @@ import { Route,Routes } from 'react-router-dom';
 import  Home from './pages/Home/Home'
 import  Cart from './pages/Cart/Cart'
 import  PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer.jsx';
+import { useState } from 'react';
+import LoginPopup from './components/LoginPopup/LoginPopup.jsx';
 const App = () => {
+  const [showLogin,setShowLogin]=useState(false)
   return (
+    <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
-      <Navbar/>
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/placeorder' element={<PlaceOrder/>}/>
       </Routes>
     </div>
+    <Footer/>
+    </>
   )
 }
 
