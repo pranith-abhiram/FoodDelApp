943874ca9daa486fe7b1022b6c307acd0410338e
pranith-abhiram <pranithabhiramg@gmail.com>
2025-06-18 13:39:33 +0530

    added responsive feature

---
+++ forntend/src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import './loginPopup.css'
+import { assets } from '../../assets/assets'
+const LoginPopup = ({setShowLogin}) => {
+    const[currState,setCurrState]=useState("Sign Up")
+  return (
+    <div className='login-popup'>
+      <form className='login-popup-container'>
+        <div className='login-popup-title'>
+            <h2>{currState}</h2>
+            <img onClick={()=>setShowLogin(false)}src={assets.cross_icon} alt=""/>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default LoginPopup
